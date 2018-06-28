8e3aace83b49d5562386a66f3cd3319aaffbe2c2
Spencer Tiberi <spencertiberi@gmail.com>
2018-06-28 12:52:47 -0700

    add schedule

--- day2/pset/lexicon/lexicon.py
+++ day2/pset/lexicon/lexicon.py
@@ -17,18 +17,24 @@ def main():
             continue
         else:
             continue
+
     # Create a dict that has a rapper name and then set of words
     rappers = dict()
 
-    # Fill dict and prints name and wordcount to screen
+    # Fill dict of words with value of frequency and prints name, wordcount, etc. to screen
     for rapper in rappertexts:
         words = fill_words(rapper)
-        print(words)
-        rapper_name = rapper[:-4]
+        # Create rapper name from filefame (remove .txt and capitalize)
+        rapper_name = rapper[:-4].capitalize()
+
+        # TODO: Add comment
         rappers[rapper_name] = words
+
+        # Create a dotted line for formating based on length of the rapper's name
         line = "." * (40 - len(rapper_name))
+
         print("%s%s\n\tWord Count: %s\n\tLargest Word: %s\n\tMost Used Word: %s" % \
-              (rapper_name.capitalize(), line, len(rappers[rapper_name]), \
+              (rapper_name, line, len(rappers[rapper_name]), \
               max(rappers[rapper_name], key=len), max(rappers[rapper_name], \
               key=rappers[rapper_name].get)))
 
