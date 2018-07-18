69979f08895e1a9499b0bfe9c6fe478c3a4ab8a8
Emily Chan <emilychan@college.harvard.edu>
2018-07-18 09:37:04 -0400

    fixed typos with parantheses in quidditch

--- day2/pset/quidditch/quidditch.py
+++ day2/pset/quidditch/quidditch.py
@@ -1,8 +1,8 @@
 import cs50
 
 def main():
-    goalNum = cs50.get_int("Number of times your chasers got the quaffle through a hoop: "))
-    snitchCaught = cs50.get_int("Did your team's seeker catch the snitch? Enter 1 if true, 0 otherwise: "))
+    goalNum = cs50.get_int("Number of times your chasers got the quaffle through a hoop: ")
+    snitchCaught = cs50.get_int("Did your team's seeker catch the snitch? Enter 1 if true, 0 otherwise: ")
     score = final_score(goalNum, snitchCaught)
     print("Your team's final score is: " + str(score))
 
